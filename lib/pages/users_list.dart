6552fea9ba06e040c85670f7638971fb38910238
adnansam110 <adnansameer62@gmail.com>
2022-11-03 11:26:04 +0500

    chore remove container

--- lib/pages/users_list.dart
+++ lib/pages/users_list.dart
@@ -38,77 +38,75 @@ class _UsersListState extends State<UsersList> {
           ? const Loader()
           : Padding(
               padding: const EdgeInsets.all(12.0),
-              child: Container(
-                child: ListView(
-                  children: users.map((user) {
-                    return Card(
-                      child: Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 10.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage(user["picture"]["thumbnail"]),
-                              radius: 30,
-                              backgroundColor: Colors.grey,
-                            ),
-                            const SizedBox(width: 10.0),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(height: 10.0),
-                                Text(
-                                  user["name"]["first"].toString().length > 40
-                                      ? "${user["name"]["first"].toString().substring(0, 40)}..."
-                                      : user["name"]["first"].toString(),
-                                  style: TextStyle(
-                                    color: Colors.grey[600],
-                                    fontWeight: FontWeight.bold,
-                                    letterSpacing: 1.0,
-                                    fontFamily: "Montserrat",
-                                  ),
+              child: ListView(
+                children: users.map((user) {
+                  return Card(
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(user["picture"]["thumbnail"]),
+                            radius: 30,
+                            backgroundColor: Colors.grey,
+                          ),
+                          const SizedBox(width: 10.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 10.0),
+                              Text(
+                                user["name"]["first"].toString().length > 40
+                                    ? "${user["name"]["first"].toString().substring(0, 40)}..."
+                                    : user["name"]["first"].toString(),
+                                style: TextStyle(
+                                  color: Colors.grey[600],
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 1.0,
+                                  fontFamily: "Montserrat",
                                 ),
-                                Text(
-                                  user['email'],
-                                  style: TextStyle(
-                                    color: Colors.grey[600],
-                                    letterSpacing: 1.0,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: "Montserrat",
-                                    fontSize: 10.0,
-                                  ),
+                              ),
+                              Text(
+                                user['email'],
+                                style: TextStyle(
+                                  color: Colors.grey[600],
+                                  letterSpacing: 1.0,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: "Montserrat",
+                                  fontSize: 10.0,
                                 ),
-                                const SizedBox(height: 5.0),
-                                TextButton(
-                                    style: TextButton.styleFrom(
-                                      backgroundColor: Colors.blue[300],
+                              ),
+                              const SizedBox(height: 5.0),
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                    backgroundColor: Colors.blue[300],
+                                  ),
+                                  onPressed: () {
+                                    Navigator.pushNamed(
+                                      context,
+                                      '/user-profile',
+                                      arguments: user,
+                                    );
+                                  },
+                                  child: const Text(
+                                    "View Details",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: "Montserrat",
                                     ),
-                                    onPressed: () {
-                                      Navigator.pushNamed(
-                                        context,
-                                        '/user-profile',
-                                        arguments: user,
-                                      );
-                                    },
-                                    child: const Text(
-                                      "View Details",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: "Montserrat",
-                                      ),
-                                    ))
-                              ],
-                            ),
-                          ],
-                        ),
+                                  ))
+                            ],
+                          ),
+                        ],
                       ),
-                    );
-                  }).toList(),
-                ),
+                    ),
+                  );
+                }).toList(),
               ),
             ),
     );
